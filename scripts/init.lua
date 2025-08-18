28510290b5e6b70a432b7624f379022eab67ae53
OmegaZeron <omegazeron@yahoo.com>
2025-08-18 01:15:32 -0700

    Refactor code to conform to Pop schemas. Removed final references to old hint system

--- scripts/init.lua
+++ scripts/init.lua
@@ -11,7 +11,6 @@ ScriptHost:LoadScript("scripts/autotracking.lua")
 ScriptHost:LoadScript("scripts/locations.lua")
 
 Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/hints.json")
 
 Tracker:AddItems("items/pack_settings.json")
 Tracker:AddItems("items/labels.json")
@@ -20,10 +19,6 @@ Tracker:AddMaps("maps/maps.json")
 Tracker:AddLayouts("layouts/item_grids.json")
 Tracker:AddLayouts("layouts/tracker_layouts.json")
 Tracker:AddLayouts("layouts/broadcast.json")
-
--- if (Tracker.ActiveVariantUID:find("withouthint")) then
--- 	Tracker:FindObjectForCode("showhints").CurrentStage = 0
--- end
 
 StartLocation = OoSLocation.New("StartLocation")
 StartLocation:connect_one_way_entrance(LowerNorthHoron)

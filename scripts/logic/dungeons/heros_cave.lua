f147806641c242e46aef68d10599c39f1cbce71a
OmegaZeron <omegazeron@yahoo.com>
2026-01-08 13:24:34 -0800

    Logic update for Linked Cave to match apworld. Added OoL rule for bombchus in Linked Cave

--- scripts/logic/dungeons/heros_cave.lua
+++ scripts/logic/dungeons/heros_cave.lua
@@ -19,15 +19,7 @@ end)
 LinkedCaveLedge:connect_one_way_entrance(LinkedCaveFoyer)
 LinkedCaveFoyer:connect_one_way(LinkedCaveLedge, function() return Has(LCAltRemoved) end)
 LinkedCaveFoyer:connect_one_way_entrance(LinkedCaveF1Chest, function() return Has(Bracelet) end)
-LinkedCaveF1Chest:connect_one_way_entrance(LinkedCaveF2Keydrop, function()
-	return Any(
-		Jump2,
-		All(
-			SwitchHook,
-			MediumLogic
-		)
-	)
-end)
+LinkedCaveF1Chest:connect_one_way_entrance(LinkedCaveF2Keydrop, Jump2)
 LinkedCaveF2Keydrop:connect_one_way_entrance(LinkedCaveF2Chest, function() return LCKeyCount(1) end)
 LinkedCaveF2Chest:connect_one_way(LinkedCaveF3TorchKeyDrop, function()
 	return All(
@@ -68,25 +60,61 @@ end)
 LinkedCaveF3Chest:connect_one_way_entrance(LinkedCaveF4Chest, function() return Has(MagnetGlove) end)
 LinkedCaveF4Chest:connect_one_way(LinkedCaveF5Gauntlet, function()
 	return All(
-		Any(
-			HasAnySword,
-			FoolsOre
-		),
-		Any(
-			AnyFlute,
-			Bombs40,
-			All(
-				Bombs,
-				AccessibilityLevel.SequenceBreak
-			),
-			Bombchus20,
-			All(
-				Bombchus,
-				AccessibilityLevel.SequenceBreak
-			)
-		),
 		Jump3,
-		CanKillSpinyBeetle
+		Any(
+			All(
+				Any(
+					HasAnySword,
+					FoolsOre
+				),
+				CanKillSpinyBeetle,
+				Any(
+					AnyFlute,
+					Bombs40,
+					All(
+						Bombs,
+						AccessibilityLevel.SequenceBreak
+					),
+					Bombchus20,
+					All(
+						Bombchus,
+						AccessibilityLevel.SequenceBreak
+					)
+				)
+			),
+			All(
+				-- Use the cane press a button at the same time Link is on the button to skip the lowest wave
+				-- Counting starts at the top and goes clockwise, waves are:
+				-- 0- Spiked Beetle
+				-- 1- Gibdo
+				-- 2- Arrow Darknut
+				-- 3- Magunesu
+				-- 4- Lynel
+				-- 5- Iron Mask
+				-- 6- Pol's Voice
+				-- 7- Stalfos
+				-- We need to fight at least 5 of these 8 waves, minimal requirement is oos_can_kill_normal_enemy_no_cane
+				-- Waves 1, 5 and 7 can be beaten by it
+				-- oos_can_kill_armored_enemy can clear waves 2 and 4, skipping 0, 3 and 6
+				-- Otherwise, since we know we have embers already, we can also beat 4, but we need more seeds
+				-- (only the seeds aren't in oos_can_kill_armored_enemy)
+				-- Skip waves 0, 3 and 6 while finishing the waves 1, 5 and 7 with embers
+				-- Now there are two waves left, 4 and 2, which can be beaten by cane
+				-- A route can be wave 4 -> wave 5 (skip 3) -> wave 7 (skip 6) -> wave 1 (skip 0) -> wave 2
+				-- (With enough bombs left, it's probably easier to switch wave 6 and wave 4 as lynels hurt a lot)
+
+				CaneOfSomaria,
+				Any(
+					CanArmorKill(false, true),
+					UpgradedSatchel,
+					All(
+						Satchel,
+						AccessibilityLevel.SequenceBreak
+					)
+				),
+				HardLogic
+			)
+		)
 	)
 end)
 LinkedCaveF4Chest:connect_one_way(LinkedCaveF5BoomerangMaze, function()
@@ -94,7 +122,15 @@ LinkedCaveF4Chest:connect_one_way(LinkedCaveF5BoomerangMaze, function()
 		LCKeyCount(4),
 		Any(
 			MagicBoomerang,
-			Bombchus20
+			Bombchus20,
+			All(
+				Bombchus,
+				AccessibilityLevel.SequenceBreak
+			),
+			All(
+				HasAnySword,
+				MediumLogic
+			)
 		),
 		Jump3
 	)
